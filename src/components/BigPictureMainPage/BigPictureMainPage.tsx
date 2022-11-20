f31d2cea52142e86974af45dce1b4a41dd42995f
ShadowFlade <wolffdrugov@yandex.ru>
2022-11-20 23:38:16 +0300

    dev:added context + listener to app ref

--- src/components/BigPictureMainPage/BigPictureMainPage.tsx
+++ src/components/BigPictureMainPage/BigPictureMainPage.tsx
@@ -4,39 +4,36 @@ import bigPic from './3024w.jpg';
 import './BigPictureMainPage.scss';
 
 type BigPictureMainPage = {
-	title:string;
-}
+	title: string;
+};
 
-const BigPictureMainPage = ({title}:BigPictureMainPage) => {
+const BigPictureMainPage = ({ title }: BigPictureMainPage) => {
 	return (
 		<div className="big-picture">
-			
 			<img
-			className='big-picture__img' 
-			src={bigPic}
-			// srcSet='3024w 3024w,
-			// 9C2E854B-A72E-416A-9257-910D5EAFB3F3_180x.avif 180w'
-			// sizes="(max-width: 3204px) 2048px,
-			// (max-width: 2048px) 2048px,
-			// 800px"
-			alt="" />
+				className="big-picture__img"
+				src={bigPic}
+				// srcSet='3024w 3024w,
+				// 9C2E854B-A72E-416A-9257-910D5EAFB3F3_180x.avif 180w'
+				// sizes="(max-width: 3204px) 2048px,
+				// (max-width: 2048px) 2048px,
+				// 800px"
+				alt=""
+			/>
 			<div className="big-picture__container">
 				<Container>
 					<div className="big-picture__content">
 						<h2 className="big-picture__title">{title}</h2>
 						<div className="big-picture__button">
-							<div className="button button--shop">
-								<span className="button__text">
-									Shop Now
-								</span>
-							</div>
+							<a href="/" className="button button--shop">
+								<span className="button__text">Shop Now</span>
+							</a>
 						</div>
 					</div>
 				</Container>
 			</div>
-			
 		</div>
-	)
-}
+	);
+};
 
 export default BigPictureMainPage;
--- src/components/HeaderMenuDropdown/HeaderMenuDropdown.tsx
+++ src/components/HeaderMenuDropdown/HeaderMenuDropdown.tsx
@@ -1,27 +1,43 @@
+import { Context } from '@/context/context';
 import { nanoid } from 'nanoid';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Container from '../Container/Container';
 import HeaderMenuColumn from '../HeaderMenuColumn/HeaderMenuColumn';
 import './HeaderMenuDropdown.scss';
 
 type HeaderMenuDropdown = {
-	columns:HeaderMenuColumn[] | null;
-}
+	columns: HeaderMenuColumn[] | null;
+};
 
-const HeaderMenuDropdown = ({columns}:HeaderMenuDropdown) => {
+const HeaderMenuDropdown = ({ columns }: HeaderMenuDropdown) => {
+	const context = useContext(Context);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const appRef = context.refs.appRef;
+	if (appRef && appRef.current) {
+		appRef!.current!.onmouseenter = () => {
+			setIsOpen(true);
+		};
+	}
+
 	return (
 		<div className="header-menu-dropdown">
 			<Container>
 				<div className="header-menu-dropdown__inner">
-					{columns?.map(column=>{
-						return (
-							<HeaderMenuColumn key={nanoid()} title={column.title} items={column.items}
-						/>)
-					})}
+					{columns
+						? columns.map((column) => {
+								return (
+									<HeaderMenuColumn
+										key={nanoid()}
+										title={column.title}
+										items={column.items}
+									/>
+								);
+						  })
+						: null}
 				</div>
 			</Container>
 		</div>
-	)
-}
+	);
+};
 
 export default HeaderMenuDropdown;
