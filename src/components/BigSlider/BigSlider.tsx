f37412f1c2cb67b6a766dcb7e05b7d47f7079ca5
Shadow Flade <wolffdrugov@yandex.ru>
2022-12-06 17:25:45 +0300

    dev:added pics

--- src/components/BigSlider/BigSlider.tsx
+++ src/components/BigSlider/BigSlider.tsx
@@ -7,20 +7,53 @@ import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 import SliderBigItem from '../SliderBigItem/SliderBigItem';
 import woman from './woman.jpg';
+import woman_1 from './woman.jpg';
+import woman_2 from './woman.jpg';
+
 import woman1 from './woman1.jpg';
+
 import woman2 from './woman2.jpg';
+import woman2_1 from './woman2_1.jpg';
+import woman2_2 from './woman2_2.jpg';
+import woman2_3 from './woman2_3.jpg';
+import woman2_4 from './woman2_4.jpg';
+import woman2_5 from './woman2_5.jpg';
+import woman2_6 from './woman2_6.jpg';
+
 import woman3 from './woman3.jpg';
+import woman3_1 from './woman3_1.jpg';
+
+
 
 import woman4 from './woman4.jpg';
+import woman4_1 from './woman4_1.jpg';
+
 import woman5 from './woman5.jpg';
+import woman5_1 from './woman5_1.jpg';
+import woman5_2 from './woman5_2.jpg';
+import woman5_3 from './woman5_3.jpg';
+import woman5_4 from './woman5_4.jpg';
+import woman5_6 from './woman5_6.jpg';
+
+
 import woman6 from './woman6.jpg';
+import woman6_1 from './woman6_1.jpg';
+import woman6_2 from './woman6_2.jpg';
+import woman6_3 from './woman6_3.jpg';
+
+
 import woman7 from './woman7.jpg';
+import woman7_1 from './woman7_1.jpg';
+import woman7_2 from './woman7_2.jpg';
+import woman7_3 from './woman7_3.jpg';
+
 
 import './BigSlider.scss';
 
 const slides = [
 	{
 		img:woman,
+		extraImg:[woman_1,woman_2],
 		sizes:['S'],
 		name:'Cali Midi Dress',
 		section:'Cartel & Willow ',
@@ -28,6 +61,7 @@ const slides = [
 	},
 	{
 		img:woman1,
+		extraImg:[],
 		sizes:['S','M'],
 		name:'Cali Midi Dress',
 		section:'Cartel & Willow ',
@@ -35,6 +69,7 @@ const slides = [
 	},
 	{
 		img:woman2,
+		extraImg:[woman2_1,woman2_2,woman2_3,woman2_4,woman2_5,woman2_6],
 		sizes:['M','L'],
 		name:'Cali Midi Dress',
 		section:'Cartel & Willow ',
@@ -42,6 +77,7 @@ const slides = [
 	},
 	{
 		img:woman3,
+		extraImg:[woman3_1],
 		sizes:['XL'],
 		name:'Cali Midi Dress',
 		section:'Cartel & Willow ',
@@ -49,6 +85,7 @@ const slides = [
 	},
 	{
 		img:woman4,
+		extraImg:[woman4_1],
 		sizes:['S','M'],
 		name:'Cali Midi Dress',
 		section:'Cartel & Willow ',
@@ -56,6 +93,7 @@ const slides = [
 	},
 	{
 		img:woman5,
+		extraImg:[woman5_1,woman5_2,woman5_3,woman5_4,woman5_6],
 		sizes:['M','L'],
 		name:'Cali Midi Dress',
 		section:'Cartel & Willow ',
@@ -63,6 +101,7 @@ const slides = [
 	},
 	{
 		img:woman6,
+		extraImg:[woman6_1,woman6_2,woman6_3],
 		sizes:['XL'],
 		name:'Cali Midi Dress',
 		section:'Cartel & Willow ',
