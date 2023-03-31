56f159ef1ffe6263dfdd752dfe27c9f5de188d21
ShadowFlade <wolffdrugov@yandex.ru>
2023-03-31 22:01:58 +0300

    dev:changed side slider to side photos

--- src/components/CatalogSliderWidget/CatalogSliderWidget.tsx
+++ src/components/CatalogSliderWidget/CatalogSliderWidget.tsx
@@ -1,13 +1,13 @@
 import React, { useRef, useState } from 'react';
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-import { FreeMode, Navigation, Thumbs, Pagination } from "swiper";
-import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
-import {Swiper as SwiperType} from "swiper/types";
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { FreeMode, Navigation, Thumbs, Pagination, Scrollbar, Mousewheel } from 'swiper';
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
+import { Swiper as SwiperType } from 'swiper/types';
 import { slides } from '../BigSlider/BigSlider';
-import  "./CatalogSliderWidget.scss";
+import './CatalogSliderWidget.scss';
 import { nanoid } from 'nanoid';
 
 const CatalogSliderWidget = () => {
@@ -20,53 +20,44 @@ const CatalogSliderWidget = () => {
 				<SwiperSlide key={nanoid()}>
 					<img src={slide.img} />
 				</SwiperSlide>
-			)
-		})},
-		[]);
-	
+			);
+		});
+	}, []);
 
-	const style : {[el:string]:string} = {
-		"--swiper-navigation-color": "#fff",
-		"--swiper-pagination-color": "#fff",
-	}
-	const swipeSideSlider = (mainSlider:SwiperType) => {
+	const style: { [el: string]: string } = {
+		'--swiper-navigation-color': '#fff',
+		'--swiper-pagination-color': '#fff',
+	};
+	const swipeSideSlider = (mainSlider: SwiperType) => {
 		const sideSwiper = sideSlider.current as unknown as SwiperRef;
 		sideSwiper.swiper.slideTo(mainSlider.activeIndex);
-	}
+	};
 	return (
 		<div className="catalog-widget">
 			<div className="catalog-widget__inner">
-				<div className="catalog-widget__side-slider">
-					<Swiper
-						slidesPerView={1} 
-						style={style}
-						spaceBetween={10}
-						navigation={true}
-						thumbs={{ swiper: thumbsSwiper }}
-						modules={[FreeMode, Navigation, Thumbs]}
-						className="mySwiper2"
-						direction={'vertical'}
-						ref={sideSlider}
-					>
-						{slidesList}
-					</Swiper>
-				</div>
+				<div className="catalog-widget__side-slider">{slidesList}</div>
 				<div className="catalog-widget__slider">
 					<Swiper
 						onSlideChange={swipeSideSlider}
 						spaceBetween={10}
-						slidesPerView={4}
-						freeMode={true}
+						slidesPerView={'auto'}
+						// freeMode={true}
 						watchSlidesProgress={true}
-						modules={[FreeMode, Navigation, Thumbs]}
+						modules={[Mousewheel, FreeMode, Navigation, Scrollbar]}
 						ref={mainSlider}
 						className="mySwiper"
+						direction={'vertical'}
+						mousewheel={true}
+						scrollbar={{
+							dragSize: 0,
+							draggable: false,
+						}}
 					>
 						{slidesList}
 					</Swiper>
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 export default CatalogSliderWidget;
