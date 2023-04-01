4cc84fcdc53551c35e268e7cdab3f5749af2ee00
ShadowFlade <wolffdrugov@yandex.ru>
2023-04-01 19:05:16 +0300

    dev:on swiper change unnececcary rerender happens

--- src/components/CatalogSliderWidget/CatalogSliderWidget.tsx
+++ src/components/CatalogSliderWidget/CatalogSliderWidget.tsx
@@ -7,15 +7,22 @@ import { FreeMode, Navigation, Thumbs, Pagination, Scrollbar, Mousewheel } from 
 import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperType } from 'swiper/types';
 import { slides } from '../BigSlider/BigSlider';
-import './CatalogSliderWidget.scss';
 import { nanoid } from 'nanoid';
+import SidePhotos from '../SidePhotos/SidePhotos';
+import './CatalogSliderWidget.scss';
 
 const CatalogSliderWidget = () => {
+	const renderCount = useRef(0);
+	renderCount.current += 1;
+	console.log(renderCount.current, ' renderCount');
 	const [thumbsSwiper, setThumbsSwiper] = useState(null);
+	const sidePhotosRef = useRef<typeof slides>([]);
 	const mainSlider = useRef(null);
 	const sideSlider = useRef(null);
+	const [activeIndex, setActiveIndex] = useState(0);
 	const slidesList = React.useMemo(() => {
 		return slides.map((slide) => {
+			sidePhotosRef.current.push(slide);
 			return (
 				<SwiperSlide key={nanoid()}>
 					<img src={slide.img} />
@@ -29,28 +36,30 @@ const CatalogSliderWidget = () => {
 		'--swiper-pagination-color': '#fff',
 	};
 	const swipeSideSlider = (mainSlider: SwiperType) => {
-		const sideSwiper = sideSlider.current as unknown as SwiperRef;
-		sideSwiper.swiper.slideTo(mainSlider.activeIndex);
+		console.log(activeIndex, sidePhotosRef);
+		setActiveIndex(mainSlider.activeIndex);
 	};
 	return (
-		<div className="catalog-widget">
+		<div key={nanoid()} className="catalog-widget">
 			<div className="catalog-widget__inner">
-				<div className="catalog-widget__side-slider">{slidesList}</div>
+				<div className="catalog-widget__side-slider">
+					<SidePhotos slides={sidePhotosRef.current} activeIndex={activeIndex} />
+				</div>
 				<div className="catalog-widget__slider">
 					<Swiper
 						onSlideChange={swipeSideSlider}
-						spaceBetween={10}
-						slidesPerView={'auto'}
-						// freeMode={true}
+						spaceBetween={0}
+						slidesPerView={2}
+						freeMode={true}
 						watchSlidesProgress={true}
-						modules={[Mousewheel, FreeMode, Navigation, Scrollbar]}
+						modules={[Mousewheel, FreeMode, Navigation]}
 						ref={mainSlider}
 						className="mySwiper"
 						direction={'vertical'}
-						mousewheel={true}
-						scrollbar={{
-							dragSize: 0,
-							draggable: false,
+						mousewheel={{
+							invert: false,
+							sensitivity: 1,
+							// eventsTarget: '.mySwiper-wrapper',
 						}}
 					>
 						{slidesList}
