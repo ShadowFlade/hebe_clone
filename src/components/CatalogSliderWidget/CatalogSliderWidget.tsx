cfc07d0010deb164758f964b27d1b2ae857af649
ShadowFlade <wolffdrugov@yandex.ru>
2023-04-16 15:30:49 +0300

    catalog-widget:bug with 2 active slides

--- src/components/CatalogSliderWidget/CatalogSliderWidget.tsx
+++ src/components/CatalogSliderWidget/CatalogSliderWidget.tsx
@@ -27,7 +27,7 @@ const CatalogSliderWidget: React.FC = () => {
 	));
 
 	const mainSlides = slides.map((slide) => (
-		<SwiperSlide style={{ height: 'auto', width: '20%' }} key={slide.img}>
+		<SwiperSlide style={{ height: 'auto', width: '40%' }} key={slide.img}>
 			<div className="swiper-zoom-container">
 				<img src={slide.img} />
 			</div>
@@ -36,22 +36,13 @@ const CatalogSliderWidget: React.FC = () => {
 
 	const handleMainSlideChange = (swiper: SwiperType) => {
 		activeIndex.current = swiper.activeIndex;
-		// thumbsSwiper?.slideTo(activeIndex.current);
+		console.log(swiper.activeIndex, ' swiper active index');
 	};
 
 	const handleThumbClick = (index: number) => {
-		activeIndex.current = index;
+		activeIndex.current = index - 1;
 		const slider = mainSwiperRef?.current as unknown as SwiperType;
 		slider?.slideTo(activeIndex.current);
-	};
-
-	const renderExternal = (data: any) => {
-		const slides = data.slides.map((slide: any) => (
-			<div key={slide.key} className="swiper-slide">
-				<img src={slide.img} alt={slide.caption} />
-			</div>
-		));
-		return slides;
 	};
 
 	return (
@@ -84,15 +75,18 @@ const CatalogSliderWidget: React.FC = () => {
 						className="catalog-slider-widget__thumbs-swiper"
 						direction="vertical"
 					>
-						{sideSlides.map((slide, index) => (
-							<SwiperSlide
-								key={index}
-								onClick={() => handleThumbClick(index)}
-								className={index === activeIndex.current ? 'active' : ''}
-							>
-								{slide.props.children}
-							</SwiperSlide>
-						))}
+						{sideSlides.map((slide, index, arr) => {
+							console.log(index);
+							return (
+								<SwiperSlide
+									key={index}
+									onClick={() => handleThumbClick(index)}
+									className={index === activeIndex.current ? 'active' : ''}
+								>
+									{slide.props.children}
+								</SwiperSlide>
+							);
+						})}
 					</Swiper>
 				</div>
 			</div>
