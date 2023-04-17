d065150225b0f6e3ec00c67e85751f88d9c6954f
Shadow Flade <wolffdrugov@yandex.ru>
2023-04-17 14:00:22 +0300

    catalog-widget:working on catalog-widget-info component

--- src/components/CatalogSliderWidgetInfo/CatalogSliderWidgetInfo.tsx
+++ src/components/CatalogSliderWidgetInfo/CatalogSliderWidgetInfo.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import './CatalogSliderWIdgetInfo.scss';
+import { useState } from 'react';
+
 type ICataloSliderWidgetInfoProps = {
 	title: string;
 
@@ -29,31 +31,45 @@ const CataloSliderWidgetInfo = ({
 	returns,
 	productName,
 }: ICataloSliderWidgetInfoProps) => {
+	const [descriptionTabVisible,setDescriptionTabVisible] = useState(true);
+	const [shippingTabVisible,setShippingTabVisible] = useState(false);
+	const [returnsTabVisible,setReturnsTabVisible] = useState(false);
+
 	return (
 		<div className="catalog-widget-info">
 			<div className="catalog-widget-info__inner">
-				<p className="catalog-widget__brand">{brand}</p>
+				<p className="catalog-widget-info__brand">{brand}</p>
 
 				<h2 className="catalog-widget-info__name">{productName}</h2>
-				<p className="catalog-widget-info__price">{price}</p>
+				<p className="catalog-widget-info__price">
+					{price}
+					<div className="catalog-widget-info__labels">
+						{labels.map((item) => {
+							return <span className="catalog-widget-info__label">{item}</span>;
+						})}
+					</div>	
+				</p>
 
-				<div className="catalog-widget-info__labels">
-					{labels.map((item) => {
-						return <span className="catalog-widget-info__label">{item}</span>;
-					})}
-				</div>
+
 				<div className="catalog-widget-info__color">
 					Color-{color}
 					<span
 						style={{ backgroundColor: color }}
-						className="catalog-widget__color-circle"
+						className="catalog-widget-info__color-circle"
 					></span>
 				</div>
 
 				<button className="catalog-widget-info__availability">
 					{isAvailable ? 'Buy' : 'Sold Out'}
 				</button>
-				<div className="catalog-widget-info__information"></div>
+				<div className="catalog-widget-info__information">
+					<div className="catalog-widget-info__tabs">
+						<span className="catalog-widget-info__tab">Description</span>
+						<span className="catalog-widget-info__tab">Shipping</span>
+						<span className="catalog-widget-info__tab">Returns</span>
+					</div>
+					<p className="catalog-widget-info__text"></p>	
+				</div>
 			</div>
 		</div>
 	);
